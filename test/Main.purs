edbaa425ada44908debb9e92e3bb83dcbb116b32
roryc89 <roryc89@googlemail.com>
2018-03-13 11:08:02 +0000

    removes redundant code

--- test/Main.purs
+++ test/Main.purs
@@ -5,12 +5,11 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.AVar (AVAR)
 import Control.Monad.Eff.Console (CONSOLE)
-import Test.Puregres.OldSelect as OldSelect
 import Test.Puregres.Select as Select
 import Test.Puregres.Insert as Insert
 import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
--- main :: forall e. Eff (console :: CONSOLE | e) Unit
+
 main :: forall t1.
   Eff
     ( console :: CONSOLE
@@ -20,6 +19,5 @@ main :: forall t1.
     )
     Unit
 main = runTest do
-  OldSelect.runTest
   Select.runTest
   Insert.runTest
